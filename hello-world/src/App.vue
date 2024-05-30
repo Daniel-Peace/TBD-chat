cbefda42b99c0b0a461aa45b52fa4585ab6dc376
Skyler Atchison <skyler.atchison145@myci.csuci.edu>
2024-05-30 16:03:21 -0700

    Added css styles from MDN tutorial

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -23,7 +23,7 @@ function addToDo(toDoLabel: string) {
   <div id="app">
     <h1>To-Do List</h1>
     <to-do-form @todo-added="addToDo"></to-do-form>
-    <ul>
+    <ul aria-labelledby="list-summary" class="stack-large">
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
       </li>
@@ -31,66 +31,115 @@ function addToDo(toDoLabel: string) {
   </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+/* Global styles */
+.btn {
+  padding: 0.8rem 1rem 0.7rem;
+  border: 0.2rem solid #4d4d4d;
+  cursor: pointer;
+  text-transform: capitalize;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.btn__danger {
+  color: #fff;
+  background-color: #ca3c3c;
+  border-color: #bd2130;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
+.btn__filter {
+  border-color: lightgrey;
+}
+.btn__danger:focus {
+  outline-color: #c82333;
+}
+.btn__primary {
+  color: #fff;
+  background-color: #000;
+}
+.btn-group {
+  display: flex;
+  justify-content: space-between;
+}
+.btn-group > * {
+  flex: 1 1 auto;
+}
+.btn-group > * + * {
+  margin-left: 0.8rem;
+}
+.label-wrapper {
+  margin: 0;
+  flex: 0 0 100%;
   text-align: center;
-  margin-top: 2rem;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
+[class*='__lg'] {
   display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  width: 100%;
+  font-size: 1.9rem;
 }
-
-nav a:first-of-type {
-  border: 0;
+[class*='__lg']:not(:last-child) {
+  margin-bottom: 1rem;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+@media screen and (min-width: 620px) {
+  [class*='__lg'] {
+    font-size: 2.4rem;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
+}
+.visually-hidden {
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(1px 1px 1px 1px);
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: rect(1px, 1px, 1px, 1px);
+  white-space: nowrap;
+}
+[class*='stack'] > * {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.stack-small > * + * {
+  margin-top: 1.25rem;
+}
+.stack-large > * + * {
+  margin-top: 2.5rem;
+}
+@media screen and (min-width: 550px) {
+  .stack-small > * + * {
+    margin-top: 1.4rem;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .stack-large > * + * {
+    margin-top: 2.8rem;
   }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+}
+/* End global styles */
+#app {
+  background: #fff;
+  margin: 2rem 0 4rem 0;
+  padding: 1rem;
+  padding-top: 0;
+  position: relative;
+  box-shadow:
+    0 2px 4px 0 rgb(0 0 0 / 20%),
+    0 2.5rem 5rem 0 rgb(0 0 0 / 10%);
+}
+@media screen and (min-width: 550px) {
+  #app {
+    padding: 4rem;
   }
+}
+#app > * {
+  max-width: 50rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+#app > form {
+  max-width: 100%;
+}
+#app h1 {
+  display: block;
+  min-width: 100%;
+  width: 100%;
+  text-align: center;
+  margin: 0;
+  margin-bottom: 1rem;
 }
 </style>
--- hello-world/src/components/ToDoForm.vue
+++ hello-world/src/components/ToDoForm.vue
@@ -14,7 +14,9 @@ function onSubmit() {
 <template>
   <!-- ".prevent" means that the default form-submission action (page refresh) will be suppressed, and only our custom function will run -->
   <form @submit.prevent="onSubmit">
-    <label for="new-todo-input"> What needs to be done? </label>
+    <h2 class="label-wrapper">
+      <label for="new-todo-input" class="label__lg"> What needs to be done? </label>
+    </h2>
     <!-- Trim means leading and ending whitespace will be removed. (I tried the property .lazy based on an MDN recommendation, but that didn't work right with Composition API) -->
     <input
       type="text"
@@ -22,7 +24,8 @@ function onSubmit() {
       name="new-todo"
       autocomplete="off"
       v-model.trim="label"
+      class="input__lg"
     />
-    <button type="submit">Add</button>
+    <button type="submit" class="btn btn__primary btn__lg">Add</button>
   </form>
 </template>

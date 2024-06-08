cc83a072bb06a02c8f6662ad924e6da9ac320d73
Skyler Atchison <skyler.atchison145@myci.csuci.edu>
2024-06-07 17:48:52 -0700

    Backend can now send POSTed data to MongoDB

--- hello-world/backend/src/HelloBackend.ts
+++ hello-world/backend/src/HelloBackend.ts
@@ -1,6 +1,59 @@
 import express from 'express'
+import { MongoClient, ServerApiVersion } from 'mongodb'
 const app = express()
 const port = 3500
+
+const mongodb_ip = '127.0.0.1'
+const mongodb_port = '27017'
+const uri = `mongodb://${mongodb_ip}:${mongodb_port}`
+
+const mongodb_client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+
+async function run() {
+  try {
+    await mongodb_client.connect()
+
+    // Send a ping to confirm a successful connection
+    const db = mongodb_client.db('hello_database')
+    await db.command({ ping: 1 })
+    console.log("Pinged your deployment. You've successfully connected to MongoDB!")
+  } finally {
+    await mongodb_client.close()
+  }
+}
+run().catch(console.dir)
+
+async function replace_user(name: string, bio: string): Promise<string> {
+  try {
+    await mongodb_client.connect()
+    const db = mongodb_client.db('hello_database')
+    const user_bios_collection = db.collection('user_bios')
+    const user_record = {
+      name: name,
+      bio: bio,
+    }
+    const query = { name: name }
+    const result = await user_bios_collection.replaceOne(query, user_record, {
+      upsert: true,
+    })
+    console.log(`Modified ${result.modifiedCount} document(s)`)
+    if (result.modifiedCount == 0) {
+      return 'New user added'
+    } else {
+      return 'User bio updated'
+    }
+  } catch (error) {
+    const error_message = `Could not replace user: ${error}`
+    console.error(error_message)
+    return error_message
+  }
+}
 
 app.use(
   express.urlencoded({
@@ -12,11 +65,12 @@ app.get('/', (req, res) => {
   res.send('Hello Backend!')
 })
 
-app.post('/api/form-handler', (req, res) => {
+app.post('/api/form-handler', async (req, res) => {
   const person_name = req.body.person_name
   const person_bio = req.body.person_bio
   console.log(`Received user form submission: Name:${person_name} Bio:${person_bio}`)
-  res.send('Form submission to be implemented')
+  const response = await replace_user(person_name, person_bio).catch(console.dir)
+  res.send(response)
 })
 
 app.listen(port, () => {
